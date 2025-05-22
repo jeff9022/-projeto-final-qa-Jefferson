cf795109e18dc3178a0e7096e9e21d047b7a2a9a
jeff9022 <jefferson.silva65592@a.fecaf.com.br>
2025-05-22 20:49:33 -0300

    Update teste_01.py

--- testes/teste_01.py
+++ testes/teste_01.py
@@ -1,1 +1,22 @@
+import unittest
+
+# Função que vamos testar
+def multiplicar(a, b):
+    return a * b
+
+# Classe de teste unitário
+class TesteMultiplicacao(unittest.TestCase):
+    
+    def test_multiplicacao_positivos(self):
+        self.assertEqual(multiplicar(2, 3), 6)
+
+    def test_multiplicacao_zero(self):
+        self.assertEqual(multiplicar(0, 5), 0)
+
+    def test_multiplicacao_negativos(self):
+        self.assertEqual(multiplicar(-2, 3), -6)
+
+# Executa os testes
+if __name__ == '__main__':
+    unittest.main(argv=[''], exit=False)
 
